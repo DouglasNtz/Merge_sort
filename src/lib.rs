c22d2deff5ed62afa6035d707b8c39f5413e0007
Douglas Nemitz <douglas.nemitz@mosaico.com.br>
2024-04-17 19:02:14 -0300

    versão beta do merge sort ... em breve incluirei testes, contagem de iterações e outros variantes do algoritmo

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+pub fn my_merge_sort<T: PartialOrd + Copy>(v: &mut Vec<T>) {
+
+    let mut v0 = Vec::with_capacity(v.len());
+
+    if v.len() == 1 {
+        v0.push(v[0]);
+    } else {
+        v0 = merge_sorted_vectors(sort_vector(&v[..(v.len()/2)]), sort_vector(&v[(v.len()/2)..]));
+    }
+    *v = v0;
+
+}
+
+fn sort_vector<T: PartialOrd + Copy>(slice: &[T]) -> Vec<T> {
+
+    let mut v0 = Vec::with_capacity(slice.len());
+
+    if slice.len() == 1 {
+        v0.push(slice[0]);
+    } else {
+        v0 = merge_sorted_vectors(sort_vector(&slice[..(slice.len()/2)]), sort_vector(&slice[(slice.len()/2)..]));
+    }
+    v0
+}
+fn merge_sorted_vectors<T: PartialOrd + Copy>(v_left: Vec<T>, v_rigth: Vec<T>) -> Vec<T> {
+
+    let mut v0 = Vec::with_capacity(v_left.len() + v_rigth.len());
+
+    let mut pos_left = 0;
+    let mut pos_rigth = 0;
+
+    loop {
+        if pos_left < v_left.len() && (pos_rigth == v_rigth.len() || v_left[pos_left] <= v_rigth[pos_rigth]) {
+            v0.push(v_left[pos_left]);
+            pos_left += 1;
+        } else if pos_rigth < v_rigth.len() {
+            v0.push(v_rigth[pos_rigth]);
+            pos_rigth += 1;
+        } else {
+            break;
+        }
+    }
+
+    v0
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io::Take;
+use rand;
+use std::time::{Duration, Instant};
+use Merge_sort::my_merge_sort;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let numero_experimentos = args[1].parse::<usize>().unwrap();
+
+    let tamanho_lista = args[2].parse::<usize>().unwrap();
+
+    let mut times = Vec::with_capacity(numero_experimentos);
+
+    let mut start_time;
+    let mut duration;
+
+    for _exp in 0..numero_experimentos {
+
+        let mut v = Vec::with_capacity(tamanho_lista);
+        for _i in 0..tamanho_lista {
+            v.push(rand::random::<i32>());
+        }
+
+        start_time = Instant::now();
+        my_merge_sort(&mut v);
+        duration = start_time.elapsed();
+
+        times.push(duration);
+
+    }
+
+
+    println!(r###"Function: my_merge_sort
+Número de experimentos: {numero_experimentos}
+Tamanho da lista de números: {tamanho_lista}
+Tempo total: {:?}
+"###, times.iter().sum::<Duration>());
+
 }
+
+
